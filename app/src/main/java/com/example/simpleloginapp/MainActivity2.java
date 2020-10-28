3ffa60803e66afcbda2f53c9ffe6aeccd5a61fd2
pavankcl <44233741+pavankcl@users.noreply.github.com>
2020-10-28 12:25:19 +0530

    Update MainActivity2.java
    few changes in native code

--- app/src/main/java/com/example/simpleloginapp/MainActivity2.java
+++ app/src/main/java/com/example/simpleloginapp/MainActivity2.java
@@ -1,9 +1,11 @@
 package com.example.simpleloginapp;
+pacakge Test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Toast;
+import Test;
 public native string  msg();
 
 public class MainActivity2 extends AppCompatActivity {
@@ -12,6 +14,8 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Test obj = new Test();
+        String  result = obj.msg();
         Toast.makeText(this, "you have been successfully logged in", Toast.LENGTH_SHORT).show();
     }
 }
